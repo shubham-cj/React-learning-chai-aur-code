28233cc067dd8c88b93b323d9c7274ca0aa6de59
shubham-cj <119930498+shubham-cj@users.noreply.github.com>
2024-02-12 21:25:47 +0530

    basic react and vite react created

---
+++ 01_basicreact/src/App.js
@@ -0,0 +1,21 @@
+import Chai from "./Chai";
+
+function App() {
+  return (
+    <>
+      <Chai />
+      <br />
+      <h1>Chai Aur React | shubham_cj</h1>
+      <h2>Today I have learned how to create react app in VS code editor and how to run the react app</h2>
+      <ul>
+        <li>To create react app first run the command in terminal that is "npx create-react-app 'name of the app'"</li>
+        <li>Then run second command that is "cd name of the app" to go inside react app folder</li>
+        <li>Then run command "npm run start" it will start the project and open  it in the browser</li>
+      </ul>
+      <h3>And that's it the react project is done</h3>
+      <h3>Now it's your turn to build your project using React</h3>
+    </>
+  );
+}
+
+export default App;
---
+++ 01_basicreact/src/Chai.jsx
@@ -0,0 +1,7 @@
+function Chai() {
+    return(
+        <h1>Chai AUR React</h1>
+    )
+}
+
+export default Chai
