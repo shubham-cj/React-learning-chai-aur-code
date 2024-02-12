32e944a6948c07553cd4588745895f2f6eb7bc76
shubham-cj <119930498+shubham-cj@users.noreply.github.com>
2024-02-13 00:32:21 +0530

    Counter value add or remove appp (Using vite react)

---
+++ 02_counter/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(2)
+  
+  const addVal = () => {
+    console.log(counter);
+    if (counter < 20) {
+      setCounter(counter+1)
+    }
+  };
+
+  const removeVal =() => {
+    console.log(counter);
+    if (counter > 0) {
+      setCounter(counter-1)
+    }
+  };
+
+  return (
+    <>
+      <h1>Chai Aur React</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button onClick={addVal}>Add Value {counter}</button>
+      <button onClick={removeVal}>Remove Value {counter}</button>
+      <br />
+      <br />
+      <footer>Footer {counter}</footer>
+    </>
+  )
+}
+
+export default App
